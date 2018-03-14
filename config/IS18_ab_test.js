6e08cf24297a7e10d150bd7b5bd4c4ad52f638dc
ZackHodari <zackhodari@gmail.com>
2018-03-14 14:13:18 +0000

    Updated ab_test config with final data and random order

--- config/IS18_ab_test.js
+++ config/IS18_ab_test.js
@@ -6,318 +6,284 @@ var TestConfig = {
   "ShowFileIDs": false,
   "ShowResults": false,
   "EnableABLoop": false,
-  "EnableOnlineSubmission": false,
+  "EnableOnlineSubmission": true,
   "BeaqleServiceURL": "http://homepages.inf.ed.ac.uk/s1653112/beaqlejs/web_service/beaqleJS_Service.php",
   "SupervisorContact": "", 
   "RequirePreference": true,
-  "RandomizeTestOrder": true,
-  "MaxTestsPerRun": 4,
+  "RandomizeTestOrder": false,
+  "MaxTestsPerRun": -1,
   "Testsets": [
-    //
     {
-      "Name": "Paragraph 35",
-      "TestID": "SecretGarden_paragraph_11",
-      "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/SecretGarden_paragraph_11.wav",
-        "B": "audio/IS18_control_space/ab_test/random/SecretGarden_paragraph_11.wav"
-      }
-    },
-    //
-    {
-      "Name": "Paragraph 36",
-      "TestID": "StoryOfCars_paragraph_02",
-      "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/StoryOfCars_paragraph_02.wav",
-        "B": "audio/IS18_control_space/ab_test/random/StoryOfCars_paragraph_02.wav"
-      }
-    },
-    //
-    {
-      "Name": "Paragraph 37",
-      "TestID": "SecretGarden_paragraph_09",
-      "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/SecretGarden_paragraph_09.wav",
-        "B": "audio/IS18_control_space/ab_test/random/SecretGarden_paragraph_09.wav"
-      }
-    },
-    //
-    {
-      "Name": "Paragraph 38",
-      "TestID": "SecretGarden_paragraph_04",
-      "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/SecretGarden_paragraph_04.wav",
-        "B": "audio/IS18_control_space/ab_test/random/SecretGarden_paragraph_04.wav"
-      }
-    },
-    //
-    {
-      "Name": "Paragraph 39",
-      "TestID": "Hamlet_paragraph_10",
-      "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/Hamlet_paragraph_10.wav",
-        "B": "audio/IS18_control_space/ab_test/random/Hamlet_paragraph_10.wav"
-      }
-    },
-    //
-    {
-      "Name": "Paragraph 40",
+      "Name": "Paragraph 1",
       "TestID": "Hamlet_paragraph_05",
       "Files": {
         "A": "audio/IS18_control_space/ab_test/baseline/Hamlet_paragraph_05.wav",
-        "B": "audio/IS18_control_space/ab_test/random/Hamlet_paragraph_05.wav"
+        "B": "audio/IS18_control_space/ab_test/custom/Hamlet_paragraph_05.wav"
       }
     },
-    //
     {
-      "Name": "Paragraph 41",
-      "TestID": "Hamlet_paragraph_01",
-      "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/Hamlet_paragraph_01.wav",
-        "B": "audio/IS18_control_space/ab_test/random/Hamlet_paragraph_01.wav"
-      }
-    },
-    //
-    {
-      "Name": "Paragraph 42",
-      "TestID": "PirateAdventures_paragraph_03",
-      "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/PirateAdventures_paragraph_03.wav",
-        "B": "audio/IS18_control_space/ab_test/random/PirateAdventures_paragraph_03.wav"
-      }
-    },
-    //
-    {
-      "Name": "Paragraph 43",
-      "TestID": "PirateAdventures_paragraph_01",
-      "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/PirateAdventures_paragraph_01.wav",
-        "B": "audio/IS18_control_space/ab_test/random/PirateAdventures_paragraph_01.wav"
-      }
-    },
-    //
-    {
-      "Name": "Paragraph 44",
-      "TestID": "Hamlet_paragraph_07",
-      "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/Hamlet_paragraph_07.wav",
-        "B": "audio/IS18_control_space/ab_test/random/Hamlet_paragraph_07.wav"
-      }
-    },
-    //
-    {
-      "Name": "Paragraph 45",
+      "Name": "Paragraph 2",
       "TestID": "SecretGarden_paragraph_01",
       "Files": {
         "A": "audio/IS18_control_space/ab_test/baseline/SecretGarden_paragraph_01.wav",
-        "B": "audio/IS18_control_space/ab_test/random/SecretGarden_paragraph_01.wav"
+        "B": "audio/IS18_control_space/ab_test/custom/SecretGarden_paragraph_01.wav"
       }
     },
-    //
     {
-      "Name": "Paragraph 46",
-      "TestID": "StoryOfChocolate_paragraph_03",
+      "Name": "Paragraph 3",
+      "TestID": "Hamlet_paragraph_01",
       "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/StoryOfChocolate_paragraph_03.wav",
-        "B": "audio/IS18_control_space/ab_test/random/StoryOfChocolate_paragraph_03.wav"
+        "A": "audio/IS18_control_space/ab_test/custom/Hamlet_paragraph_01.wav",
+        "B": "audio/IS18_control_space/ab_test/baseline/Hamlet_paragraph_01.wav"
       }
     },
-    //
     {
-      "Name": "Paragraph 47",
-      "TestID": "Hamlet_paragraph_06",
+      "Name": "Paragraph 4",
+      "TestID": "PirateAdventures_paragraph_03",
       "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/Hamlet_paragraph_06.wav",
-        "B": "audio/IS18_control_space/ab_test/random/Hamlet_paragraph_06.wav"
+        "A": "audio/IS18_control_space/ab_test/baseline/PirateAdventures_paragraph_03.wav",
+        "B": "audio/IS18_control_space/ab_test/custom/PirateAdventures_paragraph_03.wav"
       }
     },
-    //
     {
-      "Name": "Paragraph 48",
-      "TestID": "StoryOfChocolate_paragraph_02",
+      "Name": "Paragraph 5",
+      "TestID": "StoryOfCars_paragraph_02",
       "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/StoryOfChocolate_paragraph_02.wav",
-        "B": "audio/IS18_control_space/ab_test/random/StoryOfChocolate_paragraph_02.wav"
+        "A": "audio/IS18_control_space/ab_test/baseline/StoryOfCars_paragraph_02.wav",
+        "B": "audio/IS18_control_space/ab_test/custom/StoryOfCars_paragraph_02.wav"
       }
     },
-    //
     {
-      "Name": "Paragraph 49",
-      "TestID": "SecretGarden_paragraph_08",
-      "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/SecretGarden_paragraph_08.wav",
-        "B": "audio/IS18_control_space/ab_test/random/SecretGarden_paragraph_08.wav"
-      }
-    },
-    //
-    {
-      "Name": "Paragraph 50",
-      "TestID": "StoryOfChocolate_paragraph_04",
-      "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/StoryOfChocolate_paragraph_04.wav",
-        "B": "audio/IS18_control_space/ab_test/random/StoryOfChocolate_paragraph_04.wav"
-      }
-    },
-    //
-    {
-      "Name": "Paragraph 51",
+      "Name": "Paragraph 6",
       "TestID": "Hamlet_paragraph_04",
       "Files": {
         "A": "audio/IS18_control_space/ab_test/baseline/Hamlet_paragraph_04.wav",
-        "B": "audio/IS18_control_space/ab_test/random/Hamlet_paragraph_04.wav"
+        "B": "audio/IS18_control_space/ab_test/custom/Hamlet_paragraph_04.wav"
       }
     },
-    //
     {
-      "Name": "Paragraph 52",
-      "TestID": "Hamlet_paragraph_09",
+      "Name": "Paragraph 7",
+      "TestID": "SecretGarden_paragraph_09",
       "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/Hamlet_paragraph_09.wav",
-        "B": "audio/IS18_control_space/ab_test/random/Hamlet_paragraph_09.wav"
+        "A": "audio/IS18_control_space/ab_test/baseline/SecretGarden_paragraph_09.wav",
+        "B": "audio/IS18_control_space/ab_test/custom/SecretGarden_paragraph_09.wav"
       }
     },
-    //
     {
-      "Name": "Paragraph 53",
-      "TestID": "PirateAdventures_paragraph_04",
+      "Name": "Paragraph 8",
+      "TestID": "Hamlet_paragraph_08",
       "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/PirateAdventures_paragraph_04.wav",
-        "B": "audio/IS18_control_space/ab_test/random/PirateAdventures_paragraph_04.wav"
+        "A": "audio/IS18_control_space/ab_test/custom/Hamlet_paragraph_08.wav",
+        "B": "audio/IS18_control_space/ab_test/baseline/Hamlet_paragraph_08.wav"
       }
     },
-    //
     {
-      "Name": "Paragraph 54",
-      "TestID": "SecretGarden_paragraph_03",
+      "Name": "Paragraph 9",
+      "TestID": "StoryOfChocolate_paragraph_02",
       "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/SecretGarden_paragraph_03.wav",
-        "B": "audio/IS18_control_space/ab_test/random/SecretGarden_paragraph_03.wav"
+        "A": "audio/IS18_control_space/ab_test/baseline/StoryOfChocolate_paragraph_02.wav",
+        "B": "audio/IS18_control_space/ab_test/custom/StoryOfChocolate_paragraph_02.wav"
       }
     },
-    //
     {
-      "Name": "Paragraph 55",
-      "TestID": "PirateAdventures_paragraph_02",
-      "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/PirateAdventures_paragraph_02.wav",
-        "B": "audio/IS18_control_space/ab_test/random/PirateAdventures_paragraph_02.wav"
-      }
-    },
-    //
-    {
-      "Name": "Paragraph 56",
-      "TestID": "SecretGarden_paragraph_06",
-      "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/SecretGarden_paragraph_06.wav",
-        "B": "audio/IS18_control_space/ab_test/random/SecretGarden_paragraph_06.wav"
-      }
-    },
-    //
-    {
-      "Name": "Paragraph 57",
-      "TestID": "StoryOfChocolate_paragraph_01",
-      "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/StoryOfChocolate_paragraph_01.wav",
-        "B": "audio/IS18_control_space/ab_test/random/StoryOfChocolate_paragraph_01.wav"
-      }
-    },
-    //
-    {
-      "Name": "Paragraph 58",
-      "TestID": "StoryOfCars_paragraph_01",
-      "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/StoryOfCars_paragraph_01.wav",
-        "B": "audio/IS18_control_space/ab_test/random/StoryOfCars_paragraph_01.wav"
-      }
-    },
-    //
-    {
-      "Name": "Paragraph 59",
-      "TestID": "SecretGarden_paragraph_02",
-      "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/SecretGarden_paragraph_02.wav",
-        "B": "audio/IS18_control_space/ab_test/random/SecretGarden_paragraph_02.wav"
-      }
-    },
-    //
-    {
-      "Name": "Paragraph 60",
-      "TestID": "StoryOfCars_paragraph_05",
-      "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/StoryOfCars_paragraph_05.wav",
-        "B": "audio/IS18_control_space/ab_test/random/StoryOfCars_paragraph_05.wav"
-      }
-    },
-    //
-    {
-      "Name": "Paragraph 61",
+      "Name": "Paragraph 10",
       "TestID": "Hamlet_paragraph_03",
       "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/Hamlet_paragraph_03.wav",
-        "B": "audio/IS18_control_space/ab_test/random/Hamlet_paragraph_03.wav"
+        "A": "audio/IS18_control_space/ab_test/custom/Hamlet_paragraph_03.wav",
+        "B": "audio/IS18_control_space/ab_test/baseline/Hamlet_paragraph_03.wav"
       }
     },
-    //
     {
-      "Name": "Paragraph 62",
-      "TestID": "SecretGarden_paragraph_07",
+      "Name": "Paragraph 11",
+      "TestID": "StoryOfChocolate_paragraph_04",
       "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/SecretGarden_paragraph_07.wav",
-        "B": "audio/IS18_control_space/ab_test/random/SecretGarden_paragraph_07.wav"
+        "A": "audio/IS18_control_space/ab_test/custom/StoryOfChocolate_paragraph_04.wav",
+        "B": "audio/IS18_control_space/ab_test/baseline/StoryOfChocolate_paragraph_04.wav"
       }
     },
-    //
     {
-      "Name": "Paragraph 63",
-      "TestID": "SecretGarden_paragraph_05",
+      "Name": "Paragraph 12",
+      "TestID": "SecretGarden_paragraph_11",
       "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/SecretGarden_paragraph_05.wav",
-        "B": "audio/IS18_control_space/ab_test/random/SecretGarden_paragraph_05.wav"
+        "A": "audio/IS18_control_space/ab_test/custom/SecretGarden_paragraph_11.wav",
+        "B": "audio/IS18_control_space/ab_test/baseline/SecretGarden_paragraph_11.wav"
       }
     },
-    //
     {
-      "Name": "Paragraph 64",
-      "TestID": "SecretGarden_paragraph_10",
-      "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/SecretGarden_paragraph_10.wav",
-        "B": "audio/IS18_control_space/ab_test/random/SecretGarden_paragraph_10.wav"
-      }
-    },
-    //
-    {
-      "Name": "Paragraph 65",
-      "TestID": "StoryOfCars_paragraph_03",
-      "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/StoryOfCars_paragraph_03.wav",
-        "B": "audio/IS18_control_space/ab_test/random/StoryOfCars_paragraph_03.wav"
-      }
-    },
-    //
-    {
-      "Name": "Paragraph 66",
+      "Name": "Paragraph 13",
       "TestID": "StoryOfCars_paragraph_04",
       "Files": {
         "A": "audio/IS18_control_space/ab_test/baseline/StoryOfCars_paragraph_04.wav",
-        "B": "audio/IS18_control_space/ab_test/random/StoryOfCars_paragraph_04.wav"
+        "B": "audio/IS18_control_space/ab_test/custom/StoryOfCars_paragraph_04.wav"
       }
     },
-    //
     {
-      "Name": "Paragraph 67",
+      "Name": "Paragraph 14",
+      "TestID": "Hamlet_paragraph_09",
+      "Files": {
+        "A": "audio/IS18_control_space/ab_test/custom/Hamlet_paragraph_09.wav",
+        "B": "audio/IS18_control_space/ab_test/baseline/Hamlet_paragraph_09.wav"
+      }
+    },
+    {
+      "Name": "Paragraph 15",
+      "TestID": "SecretGarden_paragraph_06",
+      "Files": {
+        "A": "audio/IS18_control_space/ab_test/baseline/SecretGarden_paragraph_06.wav",
+        "B": "audio/IS18_control_space/ab_test/custom/SecretGarden_paragraph_06.wav"
+      }
+    },
+    {
+      "Name": "Paragraph 16",
+      "TestID": "PirateAdventures_paragraph_02",
+      "Files": {
+        "A": "audio/IS18_control_space/ab_test/baseline/PirateAdventures_paragraph_02.wav",
+        "B": "audio/IS18_control_space/ab_test/custom/PirateAdventures_paragraph_02.wav"
+      }
+    },
+    {
+      "Name": "Paragraph 17",
+      "TestID": "SecretGarden_paragraph_10",
+      "Files": {
+        "A": "audio/IS18_control_space/ab_test/baseline/SecretGarden_paragraph_10.wav",
+        "B": "audio/IS18_control_space/ab_test/custom/SecretGarden_paragraph_10.wav"
+      }
+    },
+    {
+      "Name": "Paragraph 18",
+      "TestID": "PirateAdventures_paragraph_02",
+      "Files": {
+        "A": "audio/IS18_control_space/ab_test/custom/PirateAdventures_paragraph_02.wav",
+        "B": "audio/IS18_control_space/ab_test/random/PirateAdventures_paragraph_02.wav"
+      }
+    },
+    {
+      "Name": "Paragraph 19",
+      "TestID": "SecretGarden_paragraph_10",
+      "Files": {
+        "A": "audio/IS18_control_space/ab_test/custom/SecretGarden_paragraph_10.wav",
+        "B": "audio/IS18_control_space/ab_test/random/SecretGarden_paragraph_10.wav"
+      }
+    },
+    {
+      "Name": "Paragraph 20",
+      "TestID": "StoryOfCars_paragraph_04",
+      "Files": {
+        "A": "audio/IS18_control_space/ab_test/random/StoryOfCars_paragraph_04.wav",
+        "B": "audio/IS18_control_space/ab_test/custom/StoryOfCars_paragraph_04.wav"
+      }
+    },
+    {
+      "Name": "Paragraph 21",
+      "TestID": "Hamlet_paragraph_05",
+      "Files": {
+        "A": "audio/IS18_control_space/ab_test/random/Hamlet_paragraph_05.wav",
+        "B": "audio/IS18_control_space/ab_test/custom/Hamlet_paragraph_05.wav"
+      }
+    },
+    {
+      "Name": "Paragraph 22",
+      "TestID": "SecretGarden_paragraph_06",
+      "Files": {
+        "A": "audio/IS18_control_space/ab_test/custom/SecretGarden_paragraph_06.wav",
+        "B": "audio/IS18_control_space/ab_test/random/SecretGarden_paragraph_06.wav"
+      }
+    },
+    {
+      "Name": "Paragraph 23",
+      "TestID": "Hamlet_paragraph_03",
+      "Files": {
+        "A": "audio/IS18_control_space/ab_test/random/Hamlet_paragraph_03.wav",
+        "B": "audio/IS18_control_space/ab_test/custom/Hamlet_paragraph_03.wav"
+      }
+    },
+    {
+      "Name": "Paragraph 24",
+      "TestID": "SecretGarden_paragraph_01",
+      "Files": {
+        "A": "audio/IS18_control_space/ab_test/custom/SecretGarden_paragraph_01.wav",
+        "B": "audio/IS18_control_space/ab_test/random/SecretGarden_paragraph_01.wav"
+      }
+    },
+    {
+      "Name": "Paragraph 25",
       "TestID": "Hamlet_paragraph_08",
       "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/Hamlet_paragraph_08.wav",
+        "A": "audio/IS18_control_space/ab_test/custom/Hamlet_paragraph_08.wav",
         "B": "audio/IS18_control_space/ab_test/random/Hamlet_paragraph_08.wav"
       }
     },
-    //
     {
-      "Name": "Paragraph 68",
-      "TestID": "Hamlet_paragraph_02",
+      "Name": "Paragraph 26",
+      "TestID": "StoryOfChocolate_paragraph_02",
       "Files": {
-        "A": "audio/IS18_control_space/ab_test/baseline/Hamlet_paragraph_02.wav",
-        "B": "audio/IS18_control_space/ab_test/random/Hamlet_paragraph_02.wav"
+        "A": "audio/IS18_control_space/ab_test/random/StoryOfChocolate_paragraph_02.wav",
+        "B": "audio/IS18_control_space/ab_test/custom/StoryOfChocolate_paragraph_02.wav"
       }
     },
+    {
+      "Name": "Paragraph 27",
+      "TestID": "Hamlet_paragraph_09",
+      "Files": {
+        "A": "audio/IS18_control_space/ab_test/custom/Hamlet_paragraph_09.wav",
+        "B": "audio/IS18_control_space/ab_test/random/Hamlet_paragraph_09.wav"
+      }
+    },
+    {
+      "Name": "Paragraph 28",
+      "TestID": "SecretGarden_paragraph_09",
+      "Files": {
+        "A": "audio/IS18_control_space/ab_test/random/SecretGarden_paragraph_09.wav",
+        "B": "audio/IS18_control_space/ab_test/custom/SecretGarden_paragraph_09.wav"
+      }
+    },
+    {
+      "Name": "Paragraph 29",
+      "TestID": "Hamlet_paragraph_04",
+      "Files": {
+        "A": "audio/IS18_control_space/ab_test/random/Hamlet_paragraph_04.wav",
+        "B": "audio/IS18_control_space/ab_test/custom/Hamlet_paragraph_04.wav"
+      }
+    },
+    {
+      "Name": "Paragraph 30",
+      "TestID": "StoryOfCars_paragraph_02",
+      "Files": {
+        "A": "audio/IS18_control_space/ab_test/custom/StoryOfCars_paragraph_02.wav",
+        "B": "audio/IS18_control_space/ab_test/random/StoryOfCars_paragraph_02.wav"
+      }
+    },
+    {
+      "Name": "Paragraph 31",
+      "TestID": "PirateAdventures_paragraph_03",
+      "Files": {
+        "A": "audio/IS18_control_space/ab_test/custom/PirateAdventures_paragraph_03.wav",
+        "B": "audio/IS18_control_space/ab_test/random/PirateAdventures_paragraph_03.wav"
+      }
+    },
+    {
+      "Name": "Paragraph 32",
+      "TestID": "StoryOfChocolate_paragraph_04",
+      "Files": {
+        "A": "audio/IS18_control_space/ab_test/random/StoryOfChocolate_paragraph_04.wav",
+        "B": "audio/IS18_control_space/ab_test/custom/StoryOfChocolate_paragraph_04.wav"
+      }
+    },
+    {
+      "Name": "Paragraph 33",
+      "TestID": "SecretGarden_paragraph_11",
+      "Files": {
+        "A": "audio/IS18_control_space/ab_test/custom/SecretGarden_paragraph_11.wav",
+        "B": "audio/IS18_control_space/ab_test/random/SecretGarden_paragraph_11.wav"
+      }
+    },
+    {
+      "Name": "Paragraph 34",
+      "TestID": "Hamlet_paragraph_01",
+      "Files": {
+        "A": "audio/IS18_control_space/ab_test/custom/Hamlet_paragraph_01.wav",
+        "B": "audio/IS18_control_space/ab_test/random/Hamlet_paragraph_01.wav"
+      }
+    }
   ]
 }
